67dd0a859f554444d09170b591b7216fa81c69dc
asim altayb <asomy0909@gmail.com>
2018-07-14 20:13:40 +0300

    Add files via upload

---
+++ Student System-web/tasks.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!--Meta Data For the web page-->
+  <meta charset="UTF-8">
+  <meta name="Description" content="Simple Layout">
+  <!--title-->
+  <title>tasks</title>
+  <!--External Css Pages-->
+  <link rel="stylesheet" type="text/css" href="css/normalize.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <!--External Javascript Pages-->
+  <script type="bootstrap.min.js"></script>
+  <script type="html5shiv.min.js"></script>
+  <script type="jquery-3.2.1.min.js"></script>
+
+</head>
+<body background="images/un.jpg">
+  <div class="wrapper">
+  <form class="login" action="" method="post" name="myForm2">
+    <p class="title">Welcome to Student Task Page <br/><br/>What do you want to do?</p>
+  
+    <input type="submit"  value="show Resolt" />
+    <i class="fa fa-user"></i>
+    <input type="submit"  value="Do Event" onclick="doEvents.php" />
+    <i class="fa fa-key"></i>
+    <input type="submit" value="Change Password"   />
+    <i class="fa fa-key"></i>
+    
+    <input type="submit"  value="Send" />
+    <i class="fa fa-user"></i>
+  </form>
+  <footer>
+    <a target="_self" href="#">Zero Day Team</a>
+  </footer>
+  </p>
+</div>
+</body>
+</html>
